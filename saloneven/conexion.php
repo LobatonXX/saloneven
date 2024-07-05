4c40af77b85226ba71625df28499151689b4f3f9
LobatonXX <118786978+LobatonXX@users.noreply.github.com>
2024-07-05 14:30:48 -0600

    Add files via upload

---
+++ saloneven/conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "server-brenda.mysql.database.azure.com";
+$username = "lobaton";
+$password = "Diego:brr202609";
+$dbname = "salonbd"; 
+
+// Crear conexión
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
